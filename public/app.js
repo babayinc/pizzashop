80f67b8a7a56a8b84fb52d01bec8277e6c66c10c
Alexander Maksimyak <alexander.max@gmail.com>
2016-07-19 19:26:11 +0300

    Add total to button

--- public/app.js
+++ public/app.js
@@ -10,18 +10,50 @@ function something()
 	alert(x)
 }
 
+function update_orders_inputs()
+{
+	var orders = cart_get_orders();
+	$('#orders_input').val(orders);
+}
+
+function update_orders_button()
+{
+	var text = 'Cart (' + cart_get_number_of_items() + ')';
+	$('#orders_button').val(text);
+}
+
+
 function add_to_cart(id)
 {
 	var key = 'product_' + id;
 	var x = window.localStorage.getItem(key);
 	x = x * 1 + 1;
 	window.localStorage.setItem(key, x);
-	total = 0;
-	for (var a in localStorage) {
-		if (a.includes("product_")){
-			alert(localStorage.getItem(a));
-			total = total + localStorage.getItem(a)*1;
+
+	update_orders_inputs();
+	update_orders_button();
+}
+
+function cart_get_number_of_items()
+{
+ 	total = 0;
+ 	for (var a in localStorage) {
+ 		if (a.includes("product_")){
+ 			total = total + localStorage.getItem(a)*1;
+ 		}
+ 	}
+ 	return total;
+}
+
+function cart_get_orders()
+{
+	var orders = '';
+	for (var a in localStorage)
+	{
+		if (a.includes("product_"))
+		{
+			orders += a + '=' + localStorage.getItem(a)*1 + ',';
 		}
 	}
-	alert(total);
+	return orders
 }
